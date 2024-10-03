5569f69214ad37960a52f3f5c13d201f7a332fe0
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-10-03 23:55:39 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,15 @@
+
+use college;
+create table student(
+rollno int primary key,
+name varchar(50)
+);
+
+select rollno from student;
+insert into student 
+(rollno,name)
+ values
+ (12,"Ramesh"),(13,"Arjun");
+ 
+ select * from student;
+
