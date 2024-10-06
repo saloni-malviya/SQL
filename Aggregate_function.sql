f392504a4047c56ce776e4159838bc04bb68b69b
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-10-06 22:33:35 +0530

    Add files via upload

---
+++ Aggregate_function.sql
@@ -0,0 +1,7 @@
+/* Aggregate function */
+select max(marks) from student;
+select min(marks) from student;
+select avg(marks) from student;
+select count(rollno) from student;
+ select sum(marks) from student; 
+ 
